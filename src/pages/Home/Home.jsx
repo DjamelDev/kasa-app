f9a35ad3361290b1cc921a379c604d6ab0eb83ee
Djamel <djameldev@gmail.com>
2024-04-27 14:49:50 +0200

    Nav active

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "./Home.scss";
 import Header from "../../components/Header/Header";
+import { useLocation } from "react-router-dom";
 
 const Home = () => {
+  const location = useLocation();
+    // Définir la classe conditionnellement en fonction de la page
+    const headerClass = location.pathname === '/' ? 'nav-active' : '';
   return (
     <div>
       <h1>Home</h1>
