0e08f25d2190ee869aec7c071359f0cbfd7f1b3d
Djamel <djameldev@gmail.com>
2024-04-28 17:09:34 +0200

    Error 404

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "./Error.scss";
+import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
-    <div>
-      <h1>Error</h1>
+    <div className="error">
+      <h1>404</h1>
+      <p className="oups">Oups! La page que vous demandez n'existe pas.</p>
+      <p className="back-home">
+        <Link to="/">Retourner sur la page d’accueil</Link>
+      </p>
     </div>
   );
 };
