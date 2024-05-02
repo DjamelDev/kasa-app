6d4be8e1d6dfc39b1d3494b381c01b649dc16225
Djamel <djameldev@gmail.com>
2024-05-02 12:25:57 +0200

    Création du composant FicheLogement

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import "./Card.scss";
 import datas from "../../assets/datas/logements.json";
+import { Link } from "react-router-dom";
 
 const Card = () => {
   return (
     <div className="container-card">
       <div className="card-flex">
         {datas.map((logement, index) => (
-          <div className="card" key={index}>
-            <img
-              src={logement.cover}
-              alt="illustration du logement"
-              title={logement.title}
-            />
-            <p>{logement.title}</p>
-          </div>
+          <Link to={logement.id}>
+            <div className="card" key={index}>
+              <img
+                src={logement.cover}
+                alt="illustration du logement"
+                title={logement.title}
+              />
+              <p>{logement.title}</p>
+            </div>
+          </Link>
         ))}
       </div>
     </div>
---
+++ src/components/FicheLogement/FicheLogement.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FicheLogement = () => {
+  return (
+    <div>
+      <h1>Fiche logement</h1>
+    </div>
+  );
+};
+
+export default FicheLogement;
