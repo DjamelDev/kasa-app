89c8138ad8306a1a5317b2b3bb03fac11d93edbf
Djamel <djameldev@gmail.com>
2024-05-05 17:24:59 +0200

    Titre et localisation FicheLogement

--- src/pages/Fiche-Logement/FicheLogement.jsx
+++ src/pages/Fiche-Logement/FicheLogement.jsx
@@ -7,14 +7,29 @@ import "./FicheLogement.scss";
 
 const FicheLogement = () => {
   const { id } = useParams(); // On utilise useParams de react-router-dom pour récupérer le paramètre id de l'URL
-  console.log(id);
   const getLogement = datas.find((data) => data.id === id); // Recherche les informations qui correspondent à l'identifiant
   const [logement, setLogement] = useState(getLogement);
+  console.log(logement);
   return logement ? (
     // Si un logement correspondant est trouvé, les éléments affichent l'identifiant (logement.id) et le titre (logement.title).
     <main>
       <div className="apartment-container">
         <Slideshow logement={logement} />
+        <div className="apartment-description">
+          <div className="left">
+            <h1>{logement.title}</h1>
+            <p>{logement.location}</p>
+            
+          </div>
+          <div className="right">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Reiciendis, minima labore! Error at, praesentium dolorum
+            repellendus, in aliquam ipsa accusantium dolor ullam assumenda odio
+            sapiente molestiae expedita quasi quia deleniti rerum impedit vitae
+            fugiat aperiam illum quam aliquid illo. Vel nihil voluptates illum
+            iste impedit recusandae tempora dolorem dignissimos fuga?
+          </div>
+        </div>
       </div>
     </main>
   ) : (
